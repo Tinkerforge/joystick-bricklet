35f698bbc2999794e6d824242769cad82c30d7bf
Olaf Lüke <olaf@tinkerforge.com>
2011-09-13 19:03:29 +0200

    add java bindings and examples

---
+++ software/bindings/java/ExampleCallback.java
@@ -0,0 +1,34 @@
+import com.tinkerforge.BrickletJoystick;
+import com.tinkerforge.IPConnection;
+
+public class ExampleCallback {
+	private static final String host = new String("localhost");
+	private static final int port = 4223;
+	private static final String UID = new String("ABC"); // Change to your UID
+	
+	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+	//       might normally want to catch are described in the commnents below
+	public static void main(String args[]) throws Exception {
+		IPConnection ipcon = new IPConnection(host, port); // Create connection to brickd (Can throw IOException)
+
+		BrickletJoystick joy = new BrickletJoystick(UID); // Create device object
+		ipcon.addDevice(joy); // Add device to ip connection (Can throw IPConnection.TimeoutException)
+		// Don't use device before it is added to a connection
+		
+
+		// Add and implement listener for pressed and released events
+		joy.addListener(new BrickletJoystick.PressedListener() {
+			public void pressed() {
+				System.out.println("Pressed");
+			}
+		});
+		joy.addListener(new BrickletJoystick.ReleasedListener() {
+			public void released() {
+				System.out.println("Released");
+			}
+		});
+		
+		System.out.println("Press ctrl+c to exit");
+		ipcon.joinThread();
+	}
+}
---
+++ software/bindings/java/ExampleFindCorners.java
@@ -0,0 +1,46 @@
+import com.tinkerforge.BrickletJoystick;
+import com.tinkerforge.IPConnection;
+
+public class ExampleFindCorners {
+	private static final String host = new String("localhost");
+	private static final int port = 4223;
+	private static final String UID = new String("ABC"); // Change to your UID
+	
+	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+	//       might normally want to catch are described in the commnents below
+	public static void main(String args[]) throws Exception {
+		IPConnection ipcon = new IPConnection(host, port); // Create connection to brickd (Can throw IOException)
+
+		BrickletJoystick joy = new BrickletJoystick(UID); // Create device object
+		ipcon.addDevice(joy); // Add device to ip connection (Can throw IPConnection.TimeoutException)
+		// Don't use device before it is added to a connection
+		
+
+		// Get threshold callbacks with a debounce time of 0.2 seconds (200ms)
+		joy.setDebouncePeriod(200);
+
+		// Configure threshold for "x and y value outside of -99 and 99"
+		joy.setPositionCallbackThreshold('o', (short)-99, (short)99, (short)-99, (short)99);
+
+		// Add and implement position reached listener (called if x and y value outside of -99 and 99)
+		joy.addListener(new BrickletJoystick.PositionReachedListener() {
+			public void positionReached(short x, short y) {
+				if(x == 100 && y == 100) {
+					System.out.println("Top Right");
+				} else if(x == -100 && y == 100) {
+					System.out.println("Top Left");
+				} else if(x == -100 && y == -100) {
+					System.out.println("Bottom Left");
+				} else if(x == 100 && y == -100) {
+					System.out.println("Bottom Right");
+				} else {
+					// This can't happen, the threshold is configured to: "outside of -99, 99"
+					System.out.println("Error");
+				}
+			}
+		});
+		
+		System.out.println("Press ctrl+c to exit");
+		ipcon.joinThread();
+	}
+}
