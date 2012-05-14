c68b0387c91dca07aea4a1e35ea4a4549949c826
Matthias Bolte <matthias@tinkerforge.com>
2012-05-14 18:51:39 +0200

    java: Remove unnecessary new String(...) calls

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -3,9 +3,9 @@ import com.tinkerforge.BrickletJoystick.Position;
 import com.tinkerforge.IPConnection;
 
 public class ExampleSimple {
-	private static final String host = new String("localhost");
+	private static final String host = "localhost";
 	private static final int port = 4223;
-	private static final String UID = new String("ABC"); // Change to your UID
+	private static final String UID = "ABC"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the commnents below
