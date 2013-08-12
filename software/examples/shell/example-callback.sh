96447af6b764ab76446ad58baadc4daf3045061c
Matthias Bolte <matthias@tinkerforge.com>
2013-08-12 19:19:00 +0200

    shell: Add note to example

--- software/examples/shell/example-callback.sh
+++ software/examples/shell/example-callback.sh
@@ -5,6 +5,8 @@
 uid=XYZ
 
 # handle incoming pressed and released callbacks
+# note: this chaining of two dispatches assumes that a pressed callback is
+#       always followed by a released callback
 tinkerforge dispatch joystick-bricklet $uid pressed\
  --execute "echo Pressed;
             tinkerforge dispatch --duration exit-after-first joystick-bricklet $uid released\
