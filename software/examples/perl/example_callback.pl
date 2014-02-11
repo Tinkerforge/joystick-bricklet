79cfeeee5a512655104326eb0a07706f831eda53
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 17:05:32 +0100

    perl: fixed namespace

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -7,8 +7,8 @@ use constant HOST => 'localhost';
 use constant PORT => 4223;
 use constant UID => '6j9'; # Change to your UID
 
-my $ipcon = IPConnection->new(); # Create IP connection
-my $js = BrickletJoystick->new(&UID, $ipcon); # Create device object
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $js = Tinkerforge::BrickletJoystick->new(&UID, $ipcon); # Create device object
 
 # Callback function for pressed and released events 
 sub cb_pressed
