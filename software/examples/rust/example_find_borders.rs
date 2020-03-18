ef177704001bf148ace5e73743c0917eccf675a6
Matthias Bolte <matthias@tinkerforge.com>
2020-03-18 14:03:03 +0100

    rust: Update to new skeleton

--- software/examples/rust/example_find_borders.rs
+++ software/examples/rust/example_find_borders.rs
@@ -1,24 +1,24 @@
 use std::{error::Error, io, thread};
 use tinkerforge::{ip_connection::IpConnection, joystick_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Joystick Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Joystick Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let j = JoystickBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let j = JoystickBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
-                                          // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get threshold receivers with a debounce time of 0.2 seconds (200ms)
+    // Get threshold receivers with a debounce time of 0.2 seconds (200ms).
     j.set_debounce_period(200);
 
     let position_reached_receiver = j.get_position_reached_callback_receiver();
 
-    // Spawn thread to handle received events.
-    // This thread ends when the `j`
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `j` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for position_reached in position_reached_receiver {
@@ -35,7 +35,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    // Configure threshold for position "outside of -99, -99 to 99, 99"
+    // Configure threshold for position "outside of -99, -99 to 99, 99".
     j.set_position_callback_threshold('o', -99, 99, -99, 99);
 
     println!("Press enter to exit.");
